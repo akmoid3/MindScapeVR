4d63e1054e174527bc2669d4ecb8dd1d2d2af7df
akmoid3 <akmoid3e@gmail.com>
2025-12-14 17:55:14 +0100

    aggiunte panchina e sdraio nelle relative scene, fixato problema con le camere

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +5,8 @@ using UnityEngine.XR.Management;
 
 public class LevelManager : MonoBehaviour
 {
-    [Header("References")] [SerializeField]
-    private GameObject freeCamObject;
-
+    [Header("References")]
+    [SerializeField] private GameObject freeCamObject;
     [SerializeField] private GameObject playerObject;
     [SerializeField] private GameObject editorUI;
     [SerializeField] private GameObject speech;
@@ -17,14 +15,13 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject mainMenuButtonsUI;
     [SerializeField] private GameObject exitPlayModeButton;
     [SerializeField] private List<GameObject> audioInSceneList = new List<GameObject>();
-
     [SerializeField] private InteractionController interactionController;
-    
+
+    private Coroutine transitionRoutine;
+
     private void Start()
     {
-        speechManager = speech.GetComponent<MindfulnessAudioManager>();
-
-        StartCoroutine(SwitchToDesktopMode());
+        speechManager = speech != null ? speech.GetComponent<MindfulnessAudioManager>() : null;
 
         StateManager.Instance.OnStateChanged += HandleStateChange;
         HandleStateChange(StateManager.Instance.CurrentState);
@@ -38,102 +35,164 @@ public class LevelManager : MonoBehaviour
 
     private void HandleStateChange(State state)
     {
-        StopAllCoroutines();
+        if (transitionRoutine != null)
+        {
+            StopCoroutine(transitionRoutine);
+            transitionRoutine = null;
+        }
 
         switch (state)
         {
             case State.Editing:
-                StartCoroutine(SwitchToDesktopMode());
-
                 editorUI.SetActive(true);
+                mainMenuButtonsUI.SetActive(false);
+                exitPlayModeButton.SetActive(false);
+
                 speech.SetActive(false);
                 if (speechManager != null) speechManager.StopSpeech();
-                exitPlayModeButton.SetActive(false);
-                mainMenuButtonsUI.SetActive(false);
-                
-                freeCamObject.SetActive(true);
-                rotatingCamera.SetActive(false);
-                playerObject.SetActive(false);
-                
-                if(interactionController != null)
-                    interactionController.EnableInteraction(true);
-                
+
+                if (interactionController != null) interactionController.EnableInteraction(true);
+
                 EnableAudioObjectCollider(true);
                 SetRandomAudioActive(false);
+
+                transitionRoutine = StartCoroutine(TransitionToDesktop(editingMode: true));
                 break;
 
             case State.Playing:
-                StartCoroutine(SwitchToVRMode());
-
-                speech.SetActive(true);
-                exitPlayModeButton.SetActive(true);
-                if (speechManager != null) speechManager.StartSpeech();
-
                 editorUI.SetActive(false);
                 mainMenuButtonsUI.SetActive(false);
+                exitPlayModeButton.SetActive(true);
 
-                freeCamObject.SetActive(false);
-                rotatingCamera.SetActive(false);
-                playerObject.SetActive(true);
-                
-                
-                if(interactionController != null)
-                    interactionController.EnableInteraction(false);
-                
+                speech.SetActive(true);
+                if (speechManager != null) speechManager.StartSpeech();
+
+                if (interactionController != null) interactionController.EnableInteraction(false);
+
                 EnableAudioObjectCollider(false);
                 SetRandomAudioActive(true);
+
+                transitionRoutine = StartCoroutine(TransitionToVR());
                 break;
 
             case State.MainMenu:
-                StartCoroutine(SwitchToDesktopMode());
-
-                rotatingCamera.SetActive(true);
-
                 editorUI.SetActive(false);
-                exitPlayModeButton.SetActive(false);
-                if (speechManager != null) speechManager.StopSpeech();
-                speech.SetActive(false);
                 mainMenuButtonsUI.SetActive(true);
+                exitPlayModeButton.SetActive(false);
 
-                freeCamObject.SetActive(false);
-                rotatingCamera.SetActive(true);
-                playerObject.SetActive(false);
-                
-                if(interactionController != null)
-                    interactionController.EnableInteraction(false);
-                
+                speech.SetActive(false);
+                if (speechManager != null) speechManager.StopSpeech();
+
+                if (interactionController != null) interactionController.EnableInteraction(false);
+
                 EnableAudioObjectCollider(false);
                 SetRandomAudioActive(false);
+
+                transitionRoutine = StartCoroutine(TransitionToDesktop(editingMode: false));
                 break;
         }
     }
 
 
+    private void DisableDesktopCamerasImmediately()
+    {
+        if (freeCamObject != null)
+        {
+            var cam = freeCamObject.GetComponentInChildren<Camera>(true);
+            if (cam != null) cam.enabled = false;
+            freeCamObject.SetActive(false);
+        }
+
+        if (rotatingCamera != null)
+        {
+            var cam = rotatingCamera.GetComponentInChildren<Camera>(true);
+            if (cam != null) cam.enabled = false;
+            rotatingCamera.SetActive(false);
+        }
+    }
+
+    private void EnableFreeCam()
+    {
+        if (freeCamObject == null) return;
+
+        freeCamObject.SetActive(true);
+        var cam = freeCamObject.GetComponentInChildren<Camera>(true);
+        if (cam != null) cam.enabled = true;
+    }
+
+    private void EnableRotatingCamera()
+    {
+        if (rotatingCamera == null) return;
+
+        rotatingCamera.SetActive(true);
+        var cam = rotatingCamera.GetComponentInChildren<Camera>(true);
+        if (cam != null) cam.enabled = true;
+    }
+
+    private IEnumerator TransitionToVR()
+    {
+        DisableDesktopCamerasImmediately();
+
+        if (playerObject != null) playerObject.SetActive(false);
+
+        yield return null;
+
+        yield return SwitchToVRMode();
+
+        yield return null;
+
+        if (playerObject != null) playerObject.SetActive(true);
+    }
+
+    private IEnumerator TransitionToDesktop(bool editingMode)
+    {
+        if (playerObject != null) playerObject.SetActive(false);
+
+        yield return SwitchToDesktopMode();
+
+        yield return null;
+
+        if (editingMode)
+        {
+            EnableFreeCam();
+            if (rotatingCamera != null) rotatingCamera.SetActive(false);
+        }
+        else
+        {
+            EnableRotatingCamera();
+            if (freeCamObject != null) freeCamObject.SetActive(false);
+        }
+    }
+
     private IEnumerator SwitchToVRMode()
     {
-
-        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        if (XRGeneralSettings.Instance != null &&
+            XRGeneralSettings.Instance.Manager != null &&
+            XRGeneralSettings.Instance.Manager.activeLoader == null)
         {
             Debug.Log("Inizializzazione VR...");
             yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
         }
 
-        if (XRGeneralSettings.Instance.Manager.activeLoader != null)
+        if (XRGeneralSettings.Instance != null &&
+            XRGeneralSettings.Instance.Manager != null &&
+            XRGeneralSettings.Instance.Manager.activeLoader != null)
         {
             XRGeneralSettings.Instance.Manager.StartSubsystems();
-
             Debug.Log("Modalità VR Attiva");
         }
         else
         {
             Debug.LogError("Errore: Visore non rilevato o inizializzazione fallita.");
-            freeCamObject.SetActive(true);
+            EnableFreeCam();
         }
     }
 
     private IEnumerator SwitchToDesktopMode()
     {
-        if (XRGeneralSettings.Instance.Manager.isInitializationComplete)
+        if (XRGeneralSettings.Instance != null &&
+            XRGeneralSettings.Instance.Manager != null &&
+            XRGeneralSettings.Instance.Manager.isInitializationComplete)
         {
             Debug.Log("Spegnimento VR...");
             XRGeneralSettings.Instance.Manager.StopSubsystems();
@@ -141,27 +200,16 @@ public class LevelManager : MonoBehaviour
         }
 
         yield return null;
+
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
         Debug.Log("Modalità Desktop Attiva (Mouse Sbloccato)");
     }
 
-
-    public void OnPlayButtonPressed()
-    {
-        StateManager.Instance.UpdateState(State.Playing);
-    }
-
-    public void OnEditButtonPressed()
-    {
-        StateManager.Instance.UpdateState(State.Editing);
-    }
-
-    public void OnMenuButtonPressed()
-    {
-        StateManager.Instance.UpdateState(State.MainMenu);
-    }
+    public void OnPlayButtonPressed() => StateManager.Instance.UpdateState(State.Playing);
+    public void OnEditButtonPressed() => StateManager.Instance.UpdateState(State.Editing);
+    public void OnMenuButtonPressed() => StateManager.Instance.UpdateState(State.MainMenu);
 
     public void AddSoundToList(GameObject soundObject)
     {
@@ -179,38 +227,36 @@ public class LevelManager : MonoBehaviour
 
     public void EnableAudioObjectCollider(bool enable)
     {
+        if (audioInSceneList == null) return;
+
         foreach (GameObject soundObject in audioInSceneList)
         {
+            if (soundObject == null) continue;
             RandomizeAudio randomize = soundObject.GetComponent<RandomizeAudio>();
-            if (randomize != null)
-                randomize.EnableCollider(enable);
+            if (randomize != null) randomize.EnableCollider(enable);
         }
     }
 
     public void SetRandomAudioActive(bool active)
     {
+        if (audioInSceneList == null) return;
+
         foreach (GameObject soundObject in audioInSceneList)
         {
+            if (soundObject == null) continue;
             RandomizeAudio randomize = soundObject.GetComponent<RandomizeAudio>();
-            if (randomize != null)
-            {
-                if (active) randomize.StartSoundRandomly();
-                else randomize.StopRandomSound();
-            }
+            if (randomize == null) continue;
+
+            if (active) randomize.StartSoundRandomly();
+            else randomize.StopRandomSound();
         }
     }
 
-    public GameObject GetFreeCam()
-    {
-        return freeCamObject;
-    }
-
+    public GameObject GetFreeCam() => freeCamObject;
 
     public void ClearAudioList()
     {
         if (audioInSceneList != null)
-        {
             audioInSceneList.Clear();
-        }
     }
 }
